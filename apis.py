3fa947eadf23e49b7ae795e9531d6f869f42a8e9
bluerelay <windymile.it@gmail.com>
2019-04-08 21:58:10 -0700

    add health check

--- apis.py
+++ apis.py
@@ -3,3 +3,6 @@ from sanic.response import json
 async def hello(request):
     msg = 'hello world'
     return json({'message': msg})
+
+async def health(request):
+    return json({'success': True})
--- routes.py
+++ routes.py
@@ -5,3 +5,4 @@ import apis
 
 routes = Blueprint('routes')
 routes.add_route(handler=apis.hello, uri='/hello', methods=['GET'])
+routes.add_route(handler=apis.health, uri='/health', methods=['GET'])
